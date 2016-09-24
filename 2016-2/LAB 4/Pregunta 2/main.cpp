e7e053900a5d26e9502275723761f5c071d393de
Ju4nx7 <a20125870@pucp.pe>
2016-09-24 19:34:51 +0000

    Pregunta 2 fixed - Lab4 2016-2

--- 2016-2/LAB 4/Pregunta 2/main.cpp
+++ 2016-2/LAB 4/Pregunta 2/main.cpp
@@ -1,13 +1,8 @@
-/* Tomairo Huamanraime Juan Junior
-    20125870 */
-
 #include <iostream>
 #include <cstdio>
 #define N 20
 
 using namespace std;
-
-
 
 /* Funciones y Procedimientos de la libreria estatica tortuga.a */
 
@@ -30,45 +25,38 @@ void mover(int*&, int, int, int, int);
 void reset_tortuga(int*&, int[]);
 
 
+/* Programa Principal */
+
 int main(){
 	
 	int comando, distancia;
 	
-	int tablero[N*N] = {};
+	int tablero[N*N] = {}; /* Inicializamos el arreglo con ceros */
 	
 	int *tortuga;
 	int pluma = 1; /* abajo */
 	int orientacion = 0; /* abajo */
 	
-	tortuga = tablero;
+	tortuga = tablero; /* Inicializamos el puntero */
 	
 	while (scanf("%d ", &comando) == 1){
 		
-		if (comando == 1){
+		if (comando == 1)
 			
 			pluma = 0;		
 			
-		}
-		else if (comando == 2){
+		else if (comando == 2)
 			
 			pluma = 1;	
 			
-			
-		}
-		else if (comando == 3){
+		else if (comando == 3)
 			
 			orientacion = girar_tortuga(orientacion, 3);
 			
-			
-			
-		}
-		else if (comando == 4){
+		else if (comando == 4)
 			
 			orientacion = girar_tortuga(orientacion, 4);
 			
-			
-			
-		}
 		else if (comando == 5){
 			
 			scanf("%d ", &distancia);
@@ -76,18 +64,10 @@ int main(){
 			if (validar_movimiento(tortuga, tablero, N, orientacion, distancia) == 1)
 				mover(tortuga, N, pluma, orientacion, distancia);
 			
-			
-			
 		}
-		else if (comando == 6){
+		else if (comando == 6)
 			
 			imprimir_tablero(tablero, N);
-			
-			
-			
-		}
-		
-		
 		
 	}
 	
